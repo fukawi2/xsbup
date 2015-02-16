fe7b0ae5ebae415344d105b8d09f438912a30cb8
Phillip Smith <fukawi2@gmail.com>
2015-02-16 14:55:44 +1100

    update logging, add verbosity and quiet modes

--- xsbup.sh
+++ xsbup.sh
@@ -26,6 +26,7 @@ set -e  # die on any error
 
 # global variables
 declare -r LOCKFILE='/var/run/snapback.pid'
+declare LOG_LEVEL=0
 
 ################################################################################
 ### FUNCTIONS ##################################################################
@@ -35,16 +36,19 @@ function take_lockfile() {
   # enable noclobber bash option then attempt to create our lock file.
   # this is much more atomic than checking for the log file first before
   # creating it.
+  logmsg 4 "Attempting to take lockfile: $LOCKFILE"
   set -C
-  echo $$ > $LOCKFILE || { "Lockfile $LOCKFILE exists, exiting!"; exit 1; }
+  echo $$ > $LOCKFILE || { logmsg 0 "Unable to take lockfile: $LOCKFILE"; return 1; }
   set +C
   # cleanup our lockfile any time we exit
   trap "rm -f $LOCKFILE; exit" INT TERM EXIT
+  logmsg 4 "Lockfile taken successfully"
   return 0
 }
 
 function renice_self() {
   declare -r my_pid="$1"
+  logmsg 4 'Adjusting nice and ionice numbers'
   renice -n 15 -p $my_pid > /dev/null
   ionice -c 2 -n 7 -p $my_pid > /dev/null
   return 0
@@ -71,10 +75,12 @@ function delete_snapshot() {
 
   # delete the associated vdi's first
   for vdi_uuid in $(xe vbd-list vm-uuid=$delete_uuid empty=false | xe_param "vdi-uuid"); do
+    logmsg 4 "Deleting snapshot VDI: $vdi_uuid"
     xe vdi-destroy uuid=$vdi_uuid > /dev/null
   done
 
   # Now we can remove the snapshot itself
+  logmsg 4 "Deleting snapshot: $delete_uuid"
   xe snapshot-uninstall uuid="$delete_uuid" force=true > /dev/null
   return $?
 }
@@ -109,6 +115,7 @@ function prepare_vm_for_backup() {
     declare snapshot_uuid=$(xe vm-snapshot vm="$vm_name" new-name-label="$snapshot_name")
     printf "$snapshot_uuid"
   fi
+  return 0
 }
 
 function do_xva_backup() {
@@ -120,6 +127,7 @@ function do_xva_backup() {
 
   # does our destination exist?
   if [ ! -d "$xva_path" ] ; then
+    logmsg 4 "Creating missing destination: $xva_path"
     mkdir "$xva_path"
   fi
 
@@ -127,17 +135,22 @@ function do_xva_backup() {
   # can't do this backup because `xe vm-export` will barf.
   declare rolled_backup_fname="$xva_path/${xva_fname}.prev"
   if [ -e "$xva_path/$xva_fname" ] ; then
+    logmsg 4 "Rolling previous backup to ${rolled_backup_fname}"
     mv -f "$xva_path/${xva_fname}" "${rolled_backup_fname}"
   fi
 
   # create a XVA file from the snapshot
+  logmsg 4 "Exporting $working_snapshot ($vm_name) to $xva_path/$xva_fname"
   if xe vm-export vm="$working_snapshot" filename="$xva_path/$xva_fname" > /dev/null ; then
     # remove previous rolled backup if reqd.
+    logmsg 5 "Export completed successfully"
     if [ -e "$rolled_backup_fname" ] ; then
+      logmsg 5 "Removing rolled backup: $rolled_backup_fname"
       rm -f "$rolled_backup_fname"
     fi
     return 0
   else
+    logmsg 0 "Export of $working_snapshot ($vm_name) FAILED"
     return 1
   fi
 }
@@ -155,6 +168,7 @@ function cleanup_xva_backups() {
     sort -n | \
     head -n-$retain_num | \
     while read expired_backup; do
+      logmsg 3 "Removing expired backup: $expired_backup"
       rm -f "$expired_backup"
     done
   return 0
@@ -172,10 +186,12 @@ function dump_pool_metadata() {
 
   # pool metadata - remove first otherwise xe will complain about
   # an existing file and refuse to continue
+  logmsg 3 "Dumping pool metadata to $pool_db_fname"
   [ -f "$pool_db_fname" ] && rm -f "$pool_db_fname"
   xe pool-dump-database file-name="$pool_db_fname"
 
   # sr metadata
+  logmsg 3 "Dumping storage repository metadata to $pool_sr_fname"
   /opt/xensource/libexec/backup-sr-metadata.py -f "$pool_sr_fname"
 
   # not in the citrix backup script, but we'll dump it out so we
@@ -197,6 +213,16 @@ function get_vm_power_state() {
   return 0
 }
 
+function logmsg() {
+  declare log_level="$1"
+  shift
+  declare log_msg="$*"
+  if [ $LOG_LEVEL -ge $log_level ] ; then
+    printf "%s | %s\n" "$(date)" "$log_msg"
+  fi
+  return 0
+}
+
 function usage {
   printf "Usage: %s [options]\n" "$0"
   printf "Options:\n"
@@ -204,6 +230,8 @@ function usage {
   printf "   %-30s %-50s\n" '-n name-label'         'Optional: Backup a specfic VM only'
   printf "   %-30s %-50s\n" '-r number'             'Optional: Number of backups to retain per VM'
   printf "   %-30s %-50s\n" '-a'                    'Optional: Backup all VMs (not just running VMs)'
+  printf "   %-30s %-50s\n" '-v num'                'Verbosity level 0-9 (higher number == more output)'
+  printf "   %-30s %-50s\n" '-q'                    'Be quiet'
   printf "   %-30s %-50s\n" '-h'                    'Display this help and exit'
 }
 
@@ -218,14 +246,14 @@ function main() {
   declare vms_to_backup=    # a list of UUIDs to backup
   declare uuid_to_backup=   # either the VM UUID or the UUID of a snapshot
   declare vm_name=          # name-label of the vm currently being backed up
-  declare xva_file=         # filename (no path) of the backup destination
-  declare -i retain_cnt=3      # number of backups to retain per vm
+  declare xva_fname=        # filename (no path) of the backup destination
+  declare -i retain_cnt=3   # number of backups to retain per vm
   
   declare -r date_ymd=$(date +"%Y%m%d") # date format must be %Y%m%d so we can sort
   declare -r date_day=$(date +"%a")     # Sun, Mon, Tue etc
 
   # fetch cmdline options
-  while getopts ":hd:an:r:" opt; do
+  while getopts ":hqv:d:an:r:" opt; do
     case $opt in
       d)
         dest_path="$OPTARG"
@@ -238,6 +266,12 @@ function main() {
         ;;
       a)
         backup_all_vms='yes'
+        ;;
+      v)
+        LOG_LEVEL="$OPTARG"
+        ;;
+      q)
+        LOG_LEVEL=-1
         ;;
       h)
         usage
@@ -257,7 +291,7 @@ function main() {
   
   # is this a xenserver box?
   if [ ! -f /etc/xensource-inventory ] ; then
-    printf "This does not appear to be a XenServer host. Aborting.\n"
+    logmsg 0 'This does not appear to be a XenServer host. Aborting.'
     exit 1
   fi
   source /etc/xensource-inventory
@@ -266,15 +300,16 @@ function main() {
   # note: $INSTALLATION_UUID comes from /etc/xensource-inventory
   master_uuid=$(xe pool-list params=master --minimal)
   if [ "$master_uuid" != "$INSTALLATION_UUID" ] ; then
-    printf "xsbup must be run on the pool master and this host does not appear to be the master\n"
-    printf "\tPool master: %s\n" "$master_uuid"
-    printf "\tThis host:   %s\n" "$INSTALLATION_UUID"
+    logmsg 0 'xsbup must be run on the pool master and this host does not appear to be the master.'
+    logmsg 0 $(printf "Pool master: %n" "$master_uuid")
+    logmsg 0 $(printf "This host:   %s" "$INSTALLATION_UUID")
     exit 1
   fi
 
   # validate configuration
-  [ ! -d "$dest_path" ] && { printf "Not a valid path: %s\n" "$dest_path"; exit 1; }
-  [ ! -w "$dest_path" ] && { printf "Permission denied: %s\n" "$dest_path"; exit 1; }
+  logmsg 4 'Validating configuration'
+  [ ! -d "$dest_path" ] && { logmsg 0 $(printf "Not a valid path: %s" "$dest_path"); exit 1; }
+  [ ! -w "$dest_path" ] && { logmsg 0 $(printf "Permission denied: %s" "$dest_path"); exit 1; }
 
   # try and take our lockfile
   take_lockfile
@@ -282,63 +317,59 @@ function main() {
   # renice ourself to minimize performance impact on the system
   renice_self "$$"
 
-  printf "xsbup started at %s\n" "$(date)"
-  printf "\tBackups will be written to %s\n" "$dest_path"
+  logmsg 0 'xsbup started'
+  logmsg 0 $(printf "Backups will be written to %s" "$dest_path")
 
   # what virtual machines are we going to backup?
   if [ -n "$backup_all_vms" ] ; then
     # backup all virtual machines
-    printf "Going to backup ALL Virtual Machines\n"
+    logmsg 0 'Going to backup ALL Virtual Machines'
     declare -r vms_to_backup=$(xe vm-list is-control-domain=false | xe_param uuid)
   elif [ -n "$target_vm" ] ; then
     # backup the vm with this name-label
-    printf "Going to backup Virtual Machine '%s'\n" "$target_vm"
+    logmsg 0 $(printf 'Going to backup Virtual Machine "%s" only' "$target_vm")
     declare -r vms_to_backup=$(xe vm-list name-label="$target_vm" | xe_param uuid)
   else
     # backup only running VMs
-    printf "Going to backup all RUNNING Virtual Machines\n"
+    logmsg 0 'Going to backup all RUNNING Virtual Machines'
     declare -r vms_to_backup=$(xe vm-list power-state=running is-control-domain=false | xe_param uuid)
   fi
 
   # loop through the list of VMs and backup each one
   for vm_uuid in $vms_to_backup ; do
     vm_name="$(get_vm_name_by_uuid $vm_uuid)"
-    xva_file="${vm_name}-${date_ymd}.xva"
+    xva_fname="${vm_name}-${date_ymd}.xva"
 
-    printf "Backup for VM '%s' started at %s\n" "$vm_name" "$(date)"
+    logmsg 0 $(printf 'Backup for VM "%s" started' "$vm_name")
 
-    printf "\tPreparing VM for backup... "
+    logmsg 1 "Preparing VM for backup"
     snapshot_uuid=$(prepare_vm_for_backup "$vm_uuid" )
-    printf "Done!\n"
     if [ -n "$snapshot_uuid" ] ; then
-      printf "\tSnapshot UUID is $snapshot_uuid\n"
+      logmsg 2 "Snapshot UUID is $snapshot_uuid"
       uuid_to_backup="$snapshot_uuid"
     else
       uuid_to_backup="$vm_uuid"
     fi
     
-    printf "\tStarting export to XVA... "
-    do_xva_backup "$vm_uuid" "$uuid_to_backup" "$dest_path/$vm_name" "$xva_file"
-    printf "Done!\n"
+    logmsg 1 "Starting export to XVA"
+    do_xva_backup "$vm_uuid" "$uuid_to_backup" "$dest_path/$vm_name" "$xva_fname"
 
     if [ -n "$snapshot_uuid" ] ; then
-      printf "\tRemoving snapshot... "
+      logmsg 1 "Removing snapshot"
       delete_snapshot "$snapshot_uuid" || true
-      printf "Done!\n"
     fi
 
-    printf "\tRemoving expired backups outside retention period... "
+    logmsg 1 "Removing expired backups outside retention period"
     cleanup_xva_backups "$vm_uuid" "$retain_cnt" "$dest_path/$vm_name"
-    printf "Done!\n"
 
-    printf "Backup for VM '%s' completed at %s\n" "$vm_name" "$(date)"
+    logmsg 0 $(printf 'Backup for VM "%s" completed' "$vm_name")
   done
 
   # dump some metadata to disk with the backups
-  printf "\tDumping pool metadata... "
+  logmsg 0 'Dumping metadata'
   dump_pool_metadata "$dest_path"
-  printf "Done!\n"
 
+  logmsg 0 'xbsup complete; exiting.'
   return 0
 }
 
